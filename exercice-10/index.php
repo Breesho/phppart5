ce4b401035a2645f7a1b55ff0f400ed93cce2ee0
Breesho <felix.noel3@orange.fr>
2020-06-05 11:00:00 +0200

    correction

--- exercice-10/index.php
+++ exercice-10/index.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice 8 </title>
+    <title>Exercice 10 </title>
 </head>
 <body>
 
